4decb53e67a0808a7af3b9d7125619d1405a555a
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-26 21:34:00 +0300

    Add `scraper.py` to fetch and store characters from API

--- characters/models.py
+++ characters/models.py
@@ -20,5 +20,5 @@ class Character(models.Model):
     gender = models.CharField(max_length=32, choices=GenderChoices.choices)
     image = models.URLField(max_length=255, unique=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
---
+++ characters/scraper.py
@@ -0,0 +1,35 @@
+import requests
+
+from characters.models import Character
+from rick_and_morty_api.settings import RICK_AND_MORTY_API_CHARACTERS_URL
+
+
+def scrape_characters() -> list[Character]:
+    url_to_scrape_next = RICK_AND_MORTY_API_CHARACTERS_URL
+    characters = []
+    while url_to_scrape_next is not None:
+        characters_response = requests.get(url_to_scrape_next).json()
+        for character_dict in characters_response["results"]:
+            characters.append(
+                Character(
+                    api_id=character_dict["id"],
+                    name=character_dict["name"],
+                    status=character_dict["status"],
+                    species=character_dict["species"],
+                    gender=character_dict["gender"],
+                    image=character_dict["image"],
+                )
+            )
+        url_to_scrape_next = characters_response["info"]["next"]
+
+    return characters
+
+
+def save_characters(characters: list[Character]) -> None:
+    for character in characters:
+        character.save()
+
+
+def sync_characters_with_api() -> None:
+    characters = scrape_characters()
+    save_characters(characters)
