dbefb747668afdb1c22884b83a80d85a194e958d
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-09-02 09:34:24 +0300

    Switch database to PostgreSQL, configure `.env` for sensitive data, update dependencies, and adjust Celery settings

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# rick-and-morty" 
+# Rick and Morty
+
+### Requirements:
+1. Endpoint, which returns a random character from the world of Rick and Morty series.
+2. Endpoint gets `search_string` as an argument and returns a list of all characters, which contains the `search_string` in their names.
+3. Regularly, the app downloads data from external service to inner DB.
+4. Requests of implemented API should work with local DB (Take data from DB not from Rick & Morty API).
+
+### Technologies to use:
+1. Public API: https://rickandmortyapi.com/documentation.
+2. Use Celery as a task scheduler for data synchronization for Rick & Morty API.
+3. Python, Django/Flask/FastAPI, ORM, PostgreSQL, Git.
+4. All endpoints should be documented via Swagger.
+
+### How to run:
+- Create virtual environment: `python -m venv venv`
+- Activate virtual environment: `venv\Scripts\activate` on Windows, `source venv/bin/activate` on Linux or MacOS.
+- Install dependencies: `pip install -r requirements.txt`
+- Create a new Postgres DB and User
+- Rename `env_sample` to `.env` and fill in the values
+- Create superuser: `python manage.py createsuperuser`
+- Run migrations: `python manage.py migrate`
+- Run Redis server: `docker run -d -p 6379:6379 redis`
+- Run Celery worker for tasks handling: `celery -A rick_and_morty_api worker -l INFO`
+- Run Celery beat for tasks scheduling: `celery -A rick_and_morty beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler`
+- Create a schedule for running sync in DB
+- Run app in Django server: `python manage.py runserver`
+
--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@io_49$qt(pldo6vx*-hadq*2uq4uwft0$k6xvuiudvexngno'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +78,21 @@ WSGI_APPLICATION = 'rick_and_morty_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT")
     }
 }
 
@@ -134,8 +149,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 # Celery Configuration Options
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 CELERY_TIMEZONE = "Europe/Kyiv"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
